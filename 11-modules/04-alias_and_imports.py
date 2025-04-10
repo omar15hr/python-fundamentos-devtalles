59f8dbbf2610d8e332a9f62c03cc0a422a71ed30
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-10 09:37:32 -0400

    modulos y paquetes, virtual env pero no subido al repo

---
+++ 11-modules/04-alias_and_imports.py
@@ -0,0 +1,20 @@
+
+# toda la libreria
+# import math
+# print(math.sqrt(16))
+
+# Importar función específica
+# from datetime import datetime
+# print(datetime.now())
+
+# Importar con alias
+# import random as r
+# print(r.randint(1, 10))
+
+
+# from math import sin, cos, pi
+# print(sin(pi/2))
+# print(cos(0))
+
+# from math import *
+# print(sqrt(25))
---
+++ 11-modules/05-pip.py
@@ -0,0 +1,5 @@
+
+from cowpy import cow
+
+vaca = cow.Cowacter()
+print(vaca.milk("Python es genial!"))
